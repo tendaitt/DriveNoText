ae7e9c6c1e3ca72efda85b76e4db4344e98f747a
tendaitt <tendaitt@gmail.com>
2013-11-12 00:21:17 -0500

    added code to send sms

--- src/com/raven/drivenotext/HomeActivity.java
+++ src/com/raven/drivenotext/HomeActivity.java
@@ -1,11 +1,19 @@
 package com.raven.drivenotext;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
+@SuppressLint("ShowToast")
 public class HomeActivity extends Activity {
 
+	private  Switch activationSwitch;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,4 +27,34 @@ public class HomeActivity extends Activity {
 		return true;
 	}
 
+	public void sendAMessage(View view){
+		Intent goToSendMessage = new Intent(this, SendMessageActivity.class);
+		startActivity(goToSendMessage);
+	}
+	
+	public void setAMessage(View view){
+		Intent gotToSetMessage = new Intent(this, SetMessageActivity.class);
+		startActivity(gotToSetMessage);
+	}
+
+	public void viewCurrentMessage(View view){
+		Intent gotToViewMessage = new Intent(this, ViewMessageActivity.class);
+		startActivity(gotToViewMessage);
+	}
+	
+	public void switchOnOff(View view){
+		activationSwitch = (Switch) findViewById(R.id.driveNoTextSwitch);
+		if(activationSwitch.isChecked()){
+			Toast toast = Toast.makeText(getApplicationContext(), "DriveNoText Has Been Activated", 3);
+			toast.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
+			toast.show(); 
+		}
+		else{
+			Toast toast = Toast.makeText(getApplicationContext(), "DriveNoText Has Been Deactivated", 3);
+			toast.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
+			toast.show(); 
+		}
+		
+		
+	}
 }
---
+++ src/com/raven/drivenotext/SetMessageActivity.java
@@ -0,0 +1,84 @@
+package com.raven.drivenotext;
+
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SetMessageActivity extends Activity {
+ private Spinner messageSpinner;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_set_message);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	
+				messageSpinner = (Spinner) findViewById(R.id.defaultMessagesSpinner);
+
+				// Create an ArrayAdapter using the string array and a default spinner layout
+				ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.default_messages_array, android.R.layout.simple_spinner_item);
+
+				// Specify the layout to use when the list of choices appears
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+				// Apply the adapter to the spinner
+				messageSpinner.setAdapter(adapter);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.set_message, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@SuppressLint("ShowToast")
+	public void setMessage(View view){
+		Toast toast = Toast.makeText(getApplicationContext(), "Your message has been set! :)", 3);
+		toast.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
+		toast.show(); 
+		Intent goBackHome = new Intent(this,HomeActivity.class);
+		startActivity(goBackHome);
+		
+	}
+
+}
